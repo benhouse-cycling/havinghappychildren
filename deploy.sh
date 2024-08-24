3dd5fb0c5d8f109e8e4e75152f923cb6ce78cb20
Gilgongo <jonathan@bakerbates.com>
2024-08-24 10:36:14 +0100

    Update

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,6 @@
 # Clone repo to /home/gilgongo/repos/happy first!
 # This will remove all exluded files from the docroot.
 
+git pull
 rsync -a --delete --delete-excluded --exclude-from=.exclude * \
 /home/gilgongo/html/bb2/happy/
